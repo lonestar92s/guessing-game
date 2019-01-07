9b8d757d090fc895333548d5a27b92bef54af0cd
Andrew Aluko <aluko17@Andrews-MacBook-Air.local>
2019-01-07 14:05:58 -0600

    updates

--- app.js
+++ app.js
@@ -5,18 +5,18 @@
 
 let randomNumber = function() {
 
-    return Math.floor(Math.random() * 5) + 1
+    return Math.floor(Math.random() * 10) + 1
 
 }
 
 let x = randomNumber()
-
+console.log(x)
 //random number button
 
-$("#new-random").click(function() {
-    x = randomNumber()
-    $('.random-number').replaceWith('<h1 class="random-number">' + x + '</h1>')
-});
+// $("#new-random").click(function() {
+//     x = randomNumber()
+//     $('.random-number').replaceWith('<h1 class="random-number">' + x + '</h1>')
+// });
 
 
 
@@ -28,7 +28,7 @@ $("#new-random").click(function() {
 class Players {
     constructor(name) {
         this.name = name;
-        this.score = 0;
+        //this.score = 0;
         this.remainingGuesses = 5;
 
     }
@@ -45,22 +45,30 @@ let player2 = new Players("Player 2")
 //guess button
 
 $('#add-guess-button').click(function() {
-    let guess = $('#add-guess-input').val()
+    let guess = 'You guessed ' + $('#add-guess-input').val() 
     console.log(guess)
     let p1Guess = $('<div id="p1guess">' + guess + '</div>')
-	$("#add-guess-input").val("")
+    $("#add-guess-input").val("")
     $("#p1guess").append(p1Guess);
-    
-    console.log("Guess", guess, typeof guess)
-    console.log("Random number", x, typeof x)
 
-    if(Number(guess) === x){
+    // console.log("Guess", guess, typeof guess)
+    // console.log("Random number", x, typeof x)
+
+    if (Number(guess) === x) {
         alert("Match")
-        player1.score += 1
-    }else
-    alert("Try again")
-    
-    
-    
+        location.reload()
+
+    } else {
+        (Number(guess) !== x)
+        alert("Try Again")
+        player1.remainingGuesses -= 1;
+    };
+
+
+    if (player1.remainingGuesses <= 0) {
+        alert("Game over")
+        location.reload()
+    }
+
 
 })
